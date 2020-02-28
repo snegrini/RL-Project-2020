c234e3cbbfefbf0a4e3757daaec4a8401bb1d96c
Samuele Negrini <snegrini@outlook.com>
2020-02-28 20:18:48 +0100

    Update

--- rl_project_2020/rl_project_2020.runs/synth_1/project_reti_logiche.tcl
+++ rl_project_2020/rl_project_2020.runs/synth_1/project_reti_logiche.tcl
@@ -17,19 +17,18 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7a200tfbg484-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir /home/snegrini/rl_project_2020/rl_project_2020.cache/wt [current_project]
-set_property parent.project_path /home/snegrini/rl_project_2020/rl_project_2020.xpr [current_project]
+set_property webtalk.parent_dir /home/snegrini/Documents/RL-Project-2020-new/rl_project_2020/rl_project_2020.cache/wt [current_project]
+set_property parent.project_path /home/snegrini/Documents/RL-Project-2020-new/rl_project_2020/rl_project_2020.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language VHDL [current_project]
-set_property ip_output_repo /home/snegrini/rl_project_2020/rl_project_2020.cache/ip [current_project]
+set_property ip_output_repo /home/snegrini/Documents/RL-Project-2020-new/rl_project_2020/rl_project_2020.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_vhdl -library xil_defaultlib /home/snegrini/rl_project_2020/rl_project_2020.srcs/sources_1/new/10539341.vhd
+read_vhdl -library xil_defaultlib /home/snegrini/Documents/RL-Project-2020-new/rl_project_2020/rl_project_2020.srcs/sources_1/new/10539341.vhd
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
---
+++ rl_project_2020/rl_project_2020.srcs/sim_1/new/tb_pfrl_2020_in_wz.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/28/2020 05:42:06 PM
+-- Design Name: 
+-- Module Name: tb_pfrl_2020_in_wz - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_pfrl_2020_in_wz is
+--  Port ( );
+end tb_pfrl_2020_in_wz;
+
+architecture Behavioral of tb_pfrl_2020_in_wz is
+
+begin
+
+
+end Behavioral;
